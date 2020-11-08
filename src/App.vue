9393d6235449d933e2a52da3a9bcbd2df685e76d
lorran.mendes <lorran.mendes@pitang.com>
2020-11-08 11:14:00 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container fluid>
+      <b-row class="my-1 center">
+        <b-col sm="4">
+          <b-form-input
+            id="input-small"
+            v-model="valor"
+            size="sm"
+            type="number"
+            placeholder="Insira um valor"
+          ></b-form-input>
+        </b-col>
+        <b-col sm="2"
+          ><b-button variant="primary" @click="obterDivisoresPrimos()" size="sm"
+            >Obter Divisores</b-button
+          ></b-col
+        >
+      </b-row>
+    </b-container>
+
+    <b-container fluid>
+      <b-row class="my-1 center mt-5">
+        <b-col sm="3">
+          <b-list-group>
+            <label for="lista de divisores">Divisores</label>
+            <b-list-group-item
+              v-for="(divisor, index) in lstDivisores"
+              :key="index"
+            >
+              <b-badge variant="primary" pill>{{ divisor }}</b-badge>
+            </b-list-group-item>
+          </b-list-group>
+        </b-col>
+
+        <b-col sm="3">
+          <b-list-group>
+            <label for="lista de dos divisores primos">Divisores Primos</label>
+            <b-list-group-item
+              v-for="(primos, index) in lstNumerosPrimos"
+              :key="index"
+            >
+              <b-badge variant="primary" pill>{{ primos }}</b-badge>
+            </b-list-group-item>
+          </b-list-group>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <!--    Loading -->
+    <div v-if="isLoading" class="text-center">
+      <b-spinner
+        style="position: absolute;top: 50%;left: 50%;"
+        label="Spinning"
+      ></b-spinner>
+    </div>
+
+    <Alert ref="alertComponent"></Alert>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Alert from "./components/Alert";
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Alert
+  },
+  data: () => ({
+    lstDivisores: null,
+    lstNumerosPrimos: null,
+    valor: null,
+    isLoading: false
+  }),
+  methods: {
+    obterDivisoresPrimos() {
+      this.isLoading = true;
+      if (this.valor > 0) {
+        const _this = this;
+        const URL_TO_FETCH = `https://numbers-divisores-primos.herokuapp.com/api/v1/${this.valor}`;
+        fetch(URL_TO_FETCH, {
+          method: "get" // opcional
+        })
+          .then(function(response) {
+            response.json().then(function(data) {
+              const { lstDivisores, lstNumerosPrimos } = data;
+              if (data) {
+                _this.lstDivisores = lstDivisores;
+                _this.lstNumerosPrimos = lstNumerosPrimos;
+                _this.isLoading = false;
+              }
+            });
+          })
+          .catch(function(err) {
+            _this.isLoading = false;
+            console.error(err);
+          });
+      } else {
+        this.$refs.alertComponent.showAlert(2);
+        this.lstDivisores = [];
+        this.lstNumerosPrimos = [];
+        this.isLoading = false;
+      }
+    }
   }
 };
 </script>
@@ -24,5 +114,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.center {
+  justify-content: center;
 }
 </style>
---
+++ src/components/Alert.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="alert-position ml-5">
+    <b-alert
+      style="width:23%"
+      :show="dismissCountDown"
+      dismissible
+      variant="warning"
+      @dismissed="dismissCountDown = 0"
+      @dismiss-count-down="countDownChanged"
+    >
+      Insira um valor maior que 0
+    </b-alert>
+    <!-- <b-button @click="showAlert" variant="info" class="m-1">
+      Show alert with count-down timer
+    </b-button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dismissSecs: 5,
+      dismissCountDown: this.propSeconds
+    };
+  },
+  methods: {
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    showAlert(value) {
+      this.dismissCountDown = value;
+    }
+  }
+};
+</script>
+<style scoped>
+.alert-position {
+  display: flex !important;
+  justify-content: center !important;
+}
+</style>
